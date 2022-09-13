9a4bb72e340b56b947bd2d31a701d898271d6bbd
thomasandr <andrew.ja.thomas@gmail.com>
2022-09-13 10:42:47 -0600

    add field formating

--- src/process.py
+++ src/process.py
@@ -6,6 +6,11 @@ path = "data/raw/" + FILE_NAME
 df = pd.read_csv(path)
 
 # order column appropriately
-cols = ['first_name', 'middle_name', 'last_name', 'suffix', 'birthdate', 'application_id']
+cols = ['first_name', 'middle_name', 'last_name', 'suffix', 'birthdate', 'start_search_date', 'application_id']
+df = df[cols]
 
-# change data types
+# correctly format all fields
+df["middle_name"] = df["middle_name"].str[:1]
+df["birthdate"] = pd.to_datetime(df["birthdate"]).dt.strftime("%Y%m%d")
+df["start_search_date"] = pd.to_datetime(df["start_search_date"]).dt.strftime("%Y%m%d")
+
