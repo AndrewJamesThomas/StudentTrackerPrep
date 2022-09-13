57ed1cc000cdd20b7eede510a79d2778781ed47e
thomasandr <andrew.ja.thomas@gmail.com>
2022-09-13 14:24:34 -0600

    Add test for additional cols

--- tests/test_process.py
+++ tests/test_process.py
@@ -2,22 +2,31 @@ import pandas as pd
 import numpy as np
 from src.process import CleanStudentTracker
 
+data = CleanStudentTracker("test_data.csv")
+
 
 def test_load_data():
-    data = CleanStudentTracker("test_data.csv")
     assert isinstance(data.df, pd.DataFrame), "Dataframe did not load"
     assert data.df.shape[0] > 0, "No data loaded"
     assert data.df.shape[1] == 7, "Incorrect number of columns"
 
 
 def test_format_cols():
-    data = CleanStudentTracker("test_data.csv")
     data.format_cols()
     df = data.df
     assert df["middle_name"].apply(lambda x: x is np.nan or len(str(x)) <= 1).all(), "Incorrect Middle Name Field"
-    assert df["birthdate"].dtype=='int64', "Bithdate has incorrect datatype (should be int)"
     assert df["birthdate"].apply(lambda x: x is np.nan or len(str(x)) == 8).all(), "Birthdate in incorrect format."
-    assert df["start_search_date"].dtype == 'int64', "Search date has incorrect datatype (should be int)"
     assert df["start_search_date"].apply(lambda x: x is np.nan or len(str(x)) == 8).all(), "Search date in incorrect format."
 
+
+def test_add_additional_cols():
+    data.add_additional_cols()
+    new_cols = ["record_type", "ssn", "blank_col", "school_code", "branch_code"]
+
+    assert all([x in data.df.keys() for x in new_cols]), "check column names. Are there enough?"
+    assert all(data.df["record_type"] == "D1"), "Error in record type."
+    assert all(data.df["ssn"] == ""), "Error in SSN"
+    assert all(data.df["blank_col"] == ""), "Error in Blank Col"
+    assert all(data.df["school_code"] == "001371"), "Error in school code"
+    assert all(data.df["branch_code"] == "00"), "Error in branch code"
 
